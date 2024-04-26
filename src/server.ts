195743526e5c9639b2f5980f7c2cf4c2fc9fd8e5
Isabel RUbim <isabel_rubim@hotmail.com.br>
2024-04-25 21:25:52 -0300

    add host personalizado para o render

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
-  console.log('Server running on http://localhost:3000')
-})
+app
+  .listen({
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server running on http://localhost:3000')
+  })
